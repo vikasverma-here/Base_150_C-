3800da9cc82beddd477da3834e4afa230cd03a58
vicky <socialmedia995619@gmail.com>
2024-09-24 11:04:18 +0530

    three question dine tabe , fibonacci , prime number

---
+++ OTHERS/others.cpp
@@ -0,0 +1,71 @@
+// WAP for table 
+#include <iostream> 
+using namespace std; 
+
+// WAP for table printting 
+
+
+// int main(){
+//     int n,i;
+//     cout<<"enter a number ";
+//     cin>>n;
+//     for(i=1; i<=10 ; i++){
+//        cout<< n<<" *"<< i << " " << " = " << n*i <<endl;
+//     }
+// }
+
+
+// WAP for Prime number 
+
+//  { number ya fir 1 se divide ho ya khudse our dono number alag hona chaiye isliye sabe cota prime number 1 na hokar 2 hota }
+// int main(){
+//     int num ;
+//     cout<<"enter the number ";
+//     cin>>num;
+//     if (num<2){
+//         cout<<"Not Prime ";
+//         return 0;
+//     }
+   
+    
+//    for(int i = 2 ; i<num ; i++){
+//     if(num%i==0){
+//         cout<<"not prime ";
+//         return 0;
+        
+//     }
+//      else if(num==2){
+//       cout <<"number is prime";
+//       return 0;
+//     }
+    
+//    }
+// cout<<"prime number ";
+// return 0;
+// }
+
+
+// WAP that take a input and return the the fibonacci series 
+
+// int main(){
+//     int num , first , second , current  ;
+//     cout <<" enter the number ";
+//     cin>>num;
+//     first=0;
+//     second = 1;
+//    if (num == 1){
+//     cout << "0";
+//     return 0;
+//    }
+//    if (num == 2){
+//     cout << "1";
+//     return 0;
+//    }
+//     for(int i = 3; i<=num; i++){
+
+//          current=first+second;
+//         first = second;
+//         second = current;
+//     }
+//  cout << current;
+// }
